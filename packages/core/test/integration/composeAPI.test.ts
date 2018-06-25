2e5ca8d57922bb0ca45cc3dc97878213262b7a08
Chris Dukakis <chrisdukakis@gmail.com>
2018-06-25 14:39:36 +0300

    fix error message in tests

--- packages/core/test/integration/composeAPI.test.ts
+++ packages/core/test/integration/composeAPI.test.ts
@@ -6,11 +6,11 @@ import { getNodeInfoResponse } from './nocks/getNodeInfo'
 test('composeAPI() composes API with provider factory', async t => {
     const api: any = (composeAPI(createHttpClient) as (s?: Partial<object>) => API)()
 
-    t.deepEqual(await api.getNodeInfo(), getNodeInfoResponse, 'getNodeInfo() should resolve to correct node info')
+    t.deepEqual(await api.getNodeInfo(), getNodeInfoResponse, 'composeAPI() should compose API with provider factory')
 })
 
 test('composeAPI() composes API with default provider', async t => {
     const api: any = composeAPI()
 
-    t.deepEqual(await api.getNodeInfo(), getNodeInfoResponse, 'getNodeInfo() should resolve to correct node info')
+    t.deepEqual(await api.getNodeInfo(), getNodeInfoResponse, 'getNodeInfo() should compose API with default provider')
 })
