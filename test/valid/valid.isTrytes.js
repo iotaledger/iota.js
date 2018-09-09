d1d3aa57f7bb3879bc073cc4e59c9b5e4b83deda
Rihards gravis <rihards.gravis@iota.org>
2018-09-09 15:54:54 +0300

    Update to accept trit instead of byte array

--- test/valid/valid.isTrytes.js
+++ test/valid/valid.isTrytes.js
@@ -8,10 +8,7 @@ describe('valid.isTrytes', function() {
     var tests = [
         'JALLWDUOSTSJVL9EEHKW9YQFPBVBJAGLNKRVGSQZCGHQWEMIIILJMTHVAGVDXJVZMBAMOZTSBQNRVNLLSJMPIVGPNE',
         '123adfdsafLWDUOSTSJVL9EEHKW9YQFPBVBJAGLNKRVGSQZCGHQWEMIIILJMTHVAGVDXJVZMBAMOZTSBQNRVNLLASD',
-        1432432,
-        Array(0, 12, 99, 120),
-        Array("A", "B", true),
-        Array()
+        1432432
     ]
 
     // 0 test
@@ -32,27 +29,6 @@ describe('valid.isTrytes', function() {
     it('should be invalid trytes: ' + tests[2], function() {
 
         var isValid = valid.isTrytes(tests[2]);
-        assert.isFalse(isValid);
-    })
-
-    // 3 test
-    it('should be valid trytes: ' + tests[3], function() {
-
-        var isValid = valid.isTrytes(tests[3]);
-        assert.isTrue(isValid);
-    })
-
-    // 4 test
-    it('should be invalid trytes: ' + tests[4], function() {
-
-        var isValid = valid.isTrytes(tests[4]);
-        assert.isFalse(isValid);
-    })
-
-    // 5 test
-    it('should be invalid trytes: ' + tests[5], function() {
-
-        var isValid = valid.isTrytes(tests[5]);
         assert.isFalse(isValid);
     })
 
