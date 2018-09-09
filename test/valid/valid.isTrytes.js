00328bf1eda7942b425d8d8714d6d18ee9f5c92e
Rihards gravis <rihards.gravis@iota.org>
2018-09-09 13:49:23 +0300

    Add `iota.lib.js` to accept byte array as seed input type

--- test/valid/valid.isTrytes.js
+++ test/valid/valid.isTrytes.js
@@ -8,8 +8,10 @@ describe('valid.isTrytes', function() {
     var tests = [
         'JALLWDUOSTSJVL9EEHKW9YQFPBVBJAGLNKRVGSQZCGHQWEMIIILJMTHVAGVDXJVZMBAMOZTSBQNRVNLLSJMPIVGPNE',
         '123adfdsafLWDUOSTSJVL9EEHKW9YQFPBVBJAGLNKRVGSQZCGHQWEMIIILJMTHVAGVDXJVZMBAMOZTSBQNRVNLLASD',
-        1432432
-
+        1432432,
+        Array(0, 12, 99, 120),
+        Array("A", "B", true),
+        Array()
     ]
 
     // 0 test
@@ -30,6 +32,27 @@ describe('valid.isTrytes', function() {
     it('should be invalid trytes: ' + tests[2], function() {
 
         var isValid = valid.isTrytes(tests[2]);
+        assert.isFalse(isValid);
+    })
+
+    // 3 test
+    it('should be valid trytes: ' + tests[3], function() {
+
+        var isValid = valid.isTrytes(tests[3]);
+        assert.isTrue(isValid);
+    })
+
+    // 4 test
+    it('should be invalid trytes: ' + tests[4], function() {
+
+        var isValid = valid.isTrytes(tests[4]);
+        assert.isFalse(isValid);
+    })
+
+    // 5 test
+    it('should be invalid trytes: ' + tests[5], function() {
+
+        var isValid = valid.isTrytes(tests[5]);
         assert.isFalse(isValid);
     })
 
