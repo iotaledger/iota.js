75cdf543de718b55cd4f004b3d3bab7c02f0a4d5
Chris Dukakis <chrisdukakis@gmail.com>
2018-10-03 17:36:13 +0300

    fix typo

--- packages/pearl-diver-react-native/android/src/main/java/org/iota/PearlDiver/Interface.java
+++ packages/pearl-diver-react-native/android/src/main/java/org/iota/PearlDiver/Interface.java
@@ -1,7 +1,7 @@
 package org.iota.PearlDiver;
 
 public class Interface {
-    public static native String doPOW(String trytes, int mwm);
+    public static native String doPoW(String trytes, int mwm);
     public static native String generateAddress(String seed, int index, int security);
     public static native String generateSignature(String trytes, int index, int security, String bundleHash);
     public static native String getDigest(String trytes);
