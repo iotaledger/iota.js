a85af3c85c72839cb955475916c3b39a8d45321e
Chris Dukakis <chrisdukakis@gmail.com>
2018-10-17 17:35:03 +0300

    update method names

--- packages/pearl-diver-react-native/android/src/main/java/org/iota/PearlDiver/PearlDiverModule.java
+++ packages/pearl-diver-react-native/android/src/main/java/org/iota/PearlDiver/PearlDiverModule.java
@@ -27,7 +27,7 @@ public class PearlDiverModule extends ReactContextBaseJavaModule {
         new GuardedResultAsyncTask<String>(mContext) {
             @Override
             protected String doInBackgroundGuarded() {
-                String nonce = Interface.doPOW(trytes, mwm);
+                String nonce = Interface.iota_pow(trytes, mwm);
                 return nonce;
             }
 
--- packages/pearl-diver-react-native/android/src/main/java/org/iota/mobile/Interface.java
+++ packages/pearl-diver-react-native/android/src/main/java/org/iota/mobile/Interface.java
@@ -3,8 +3,7 @@ package org.iota.mobile;
 public class Interface {
     static { System.loadLibrary("dummy"); }
 
-    public static native String doPOW(String trytes, int mwm);
-    public static native String generateAddress(String seed, int index, int security);
-    public static native String generateSignature(String trytes, int index, int security, String bundleHash);
-    public static native String getDigest(String trytes);
+    public static native String iota_pow(String trytes, int mwm);
+    public static native String iota_sign_address_gen(String seed, int index, int security);
+    public static native String iota_sign_signature_gen(String trytes, int index, int security, String bundleHash);
 }
