458a0d83a22171416f14369b66526fd9b8cfa407
Rajiv Shah <rajivshah1@icloud.com>
2019-02-18 20:43:37 -0500

    fix(signing): iOS native signing

---
+++ packages/signing/ios/IOTASigningUtils.h
@@ -0,0 +1,36 @@
+//
+//  IOTASigningUtils.h
+//  Signing
+//
+//  Created by Rajiv Shah on 2/18/19.
+//  Copyright © 2019 IOTA Foundation. All rights reserved.
+//
+
+#ifndef IOTASigningUtils_h
+#define IOTASigningUtils_h
+
+#import <Foundation/Foundation.h>
+#import <stddef.h>
+
+@interface IOTASigningUtils : NSObject
+
+/**
+ Converts a NSMutableArray representation of trits into an int8_t representation
+ 
+ @param trits NSMutableArray representation of trits
+ @return Pointer to int8_t representation of trits
+ */
++ (int8_t*)NSMutableArrayTritsToInt8:(NSMutableArray<NSNumber*>*)trits;
+
+/**
+ Converts an int8_t representation of trits into a NSMutableArray representation
+ 
+ @param trits Pointer to int8_t representation of trits
+ @param count Number of trits
+ @return NSMutableArray representation of trits
+ */
++ (NSMutableArray<NSNumber*>*)Int8TritsToNSMutableArray:(int8_t*)trits count:(int)count;
+
+@end
+
+#endif /* IOTASigningUtils_h */
