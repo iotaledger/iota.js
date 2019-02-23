65b0be507627c8974c4d43ba85b5739a625b4d5e
Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>
2019-02-23 17:32:47 +0100

    Update validate.test.ts

--- packages/validators/test/validate.test.ts
+++ packages/validators/test/validate.test.ts
@@ -14,7 +14,7 @@ test('validate() throws correct error for invalid input.', t => {
 
     t.is(
         t.throws(() => validate(validator(value)), Error).message,
-        `${errorMessage}: ${value}`,
+        `${errorMessage}: "${value}"`,
         'validate() should throw correct error for invalid input.'
     )
 })
