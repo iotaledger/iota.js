bb98ba4b70497e2d959b6514c2f97d2b1b85eaf9
Chris Dukakis <chris.dukakis@iota.org>
2019-04-09 17:07:54 +0300

    fix(persistence): reset this context

--- packages/persistence/src/persistence.ts
+++ packages/persistence/src/persistence.ts
@@ -55,10 +55,12 @@ const events = {
     deleteCDA: 'deleteCDA',
 }
 
-export function createPersistence(
-    this: any,
-    { persistenceID, persistencePath, stateAdapter, historyAdapter }: PersistenceParams
-): Persistence<Int8Array> {
+export function createPersistence({
+    persistenceID,
+    persistencePath,
+    stateAdapter,
+    historyAdapter,
+}: PersistenceParams): Persistence<Int8Array> {
     const state = stateAdapter({ persistenceID: 'STATE-OF-' + persistenceID, persistencePath })
     const history = historyAdapter({ persistenceID: 'HISTORY-OF-' + persistenceID, persistencePath })
 
@@ -91,173 +93,177 @@ export function createPersistence(
             })
     }
 
-    return Object.assign(
-        this,
-        {
-            nextIndex: () =>
-                ready()
-                    .then(indexBuffer.read)
-                    .then(verifyIndex)
-                    .then(increment)
-                    .then(index =>
-                        state
-                            .write(prefixes.KEY_INDEX_PREFIX, tritsToBytes(index))
-                            .then(() => indexBuffer.write(index))
-                            .then(() => index)
-                            .catch(error => {
-                                // On error, restore the buffer value and rethrow.
-                                indexBuffer.write(index)
-                                throw error
-                            })
-                    ),
+    function persistenceMixin(this: any) {
+        return Object.assign(
+            this,
+            {
+                nextIndex: () =>
+                    ready()
+                        .then(indexBuffer.read)
+                        .then(verifyIndex)
+                        .then(increment)
+                        .then(index =>
+                            state
+                                .write(prefixes.KEY_INDEX_PREFIX, tritsToBytes(index))
+                                .then(() => indexBuffer.write(index))
+                                .then(() => index)
+                                .catch(error => {
+                                    // On error, restore the buffer value and rethrow.
+                                    indexBuffer.write(index)
+                                    throw error
+                                })
+                        ),
 
-            writeBundle: (buffer: Int8Array) => {
-                if (!isMultipleOfTransactionLength(buffer.length)) {
-                    throw new RangeError(errors.ILLEGAL_BUNDLE_LENGTH)
-                }
-
-                return ready()
-                    .then(() => state.write(tritsToBytes(bundle(buffer)), tritsToBytes(buffer)))
-                    .tap(() => this.emit(events.writeBundle, buffer))
-            },
-
-            deleteBundle: (buffer: Int8Array) => {
-                if (!isMultipleOfTransactionLength(buffer.length)) {
-                    throw new RangeError(errors.ILLEGAL_BUNDLE_LENGTH)
-                }
-
-                return ready()
-                    .then(() => history.write(tritsToBytes(bundle(buffer)), tritsToBytes(buffer)))
-                    .then(() => state.delete(tritsToBytes(bundle(buffer))))
-                    .tap(() => this.emit(events.deleteBundle, buffer))
-            },
-
-            readCDA: (address: Int8Array) => {
-                if (address.length !== ADDRESS_LENGTH) {
-                    throw new RangeError(errors.ILLEGAL_ADDRESS_LENGTH)
-                }
-
-                return ready()
-                    .then(() => state.read(tritsToBytes(address)))
-                    .then(bytesToTrits)
-            },
-
-            writeCDA: (cda: Int8Array) => {
-                if (cda.length !== CDA_LENGTH) {
-                    throw new RangeError(errors.ILLEGAL_CDA_LENGTH)
-                }
-
-                return ready()
-                    .then(() => state.write(tritsToBytes(CDAddress(cda)), tritsToBytes(cda)))
-                    .tap(() => this.emit(events.writeCDA, cda))
-            },
-
-            deleteCDA: (cda: Int8Array) => {
-                if (cda.length !== CDA_LENGTH) {
-                    throw new RangeError(errors.ILLEGAL_CDA_LENGTH)
-                }
-
-                return ready()
-                    .then(() => history.write(tritsToBytes(CDAddress(cda)), tritsToBytes(cda)))
-                    .then(() => state.delete(tritsToBytes(CDAddress(cda))))
-                    .tap(() => this.emit(events.deleteCDA, cda))
-            },
-
-            batch: (ops: ReadonlyArray<PersistenceBatch<Int8Array>>) => {
-                for (const { type, value } of ops) {
-                    if (!events.hasOwnProperty(type)) {
-                        throw new Error(errors.ILLEGAL_BATCH)
-                    }
-
-                    if (
-                        (type === events.writeBundle || type === events.deleteBundle) &&
-                        !isMultipleOfTransactionLength(value.length)
-                    ) {
+                writeBundle: (buffer: Int8Array) => {
+                    if (!isMultipleOfTransactionLength(buffer.length)) {
                         throw new RangeError(errors.ILLEGAL_BUNDLE_LENGTH)
                     }
 
-                    if ((type === events.writeCDA || type === events.deleteCDA) && value.length !== CDA_LENGTH) {
+                    return ready()
+                        .then(() => state.write(tritsToBytes(bundle(buffer)), tritsToBytes(buffer)))
+                        .tap(() => this.emit(events.writeBundle, buffer))
+                },
+
+                deleteBundle: (buffer: Int8Array) => {
+                    if (!isMultipleOfTransactionLength(buffer.length)) {
+                        throw new RangeError(errors.ILLEGAL_BUNDLE_LENGTH)
+                    }
+
+                    return ready()
+                        .then(() => history.write(tritsToBytes(bundle(buffer)), tritsToBytes(buffer)))
+                        .then(() => state.delete(tritsToBytes(bundle(buffer))))
+                        .tap(() => this.emit(events.deleteBundle, buffer))
+                },
+
+                readCDA: (address: Int8Array) => {
+                    if (address.length !== ADDRESS_LENGTH) {
+                        throw new RangeError(errors.ILLEGAL_ADDRESS_LENGTH)
+                    }
+
+                    return ready()
+                        .then(() => state.read(tritsToBytes(address)))
+                        .then(bytesToTrits)
+                },
+
+                writeCDA: (cda: Int8Array) => {
+                    if (cda.length !== CDA_LENGTH) {
                         throw new RangeError(errors.ILLEGAL_CDA_LENGTH)
                     }
-                }
 
-                return ready()
-                    .then(() =>
-                        history.batch(
-                            ops
-                                .filter(({ type }) => type === 'deleteBundle' || type === 'deleteCDA')
-                                .map(
+                    return ready()
+                        .then(() => state.write(tritsToBytes(CDAddress(cda)), tritsToBytes(cda)))
+                        .tap(() => this.emit(events.writeCDA, cda))
+                },
+
+                deleteCDA: (cda: Int8Array) => {
+                    if (cda.length !== CDA_LENGTH) {
+                        throw new RangeError(errors.ILLEGAL_CDA_LENGTH)
+                    }
+
+                    return ready()
+                        .then(() => history.write(tritsToBytes(CDAddress(cda)), tritsToBytes(cda)))
+                        .then(() => state.delete(tritsToBytes(CDAddress(cda))))
+                        .tap(() => this.emit(events.deleteCDA, cda))
+                },
+
+                batch: (ops: ReadonlyArray<PersistenceBatch<Int8Array>>) => {
+                    for (const { type, value } of ops) {
+                        if (!events.hasOwnProperty(type)) {
+                            throw new Error(errors.ILLEGAL_BATCH)
+                        }
+
+                        if (
+                            (type === events.writeBundle || type === events.deleteBundle) &&
+                            !isMultipleOfTransactionLength(value.length)
+                        ) {
+                            throw new RangeError(errors.ILLEGAL_BUNDLE_LENGTH)
+                        }
+
+                        if ((type === events.writeCDA || type === events.deleteCDA) && value.length !== CDA_LENGTH) {
+                            throw new RangeError(errors.ILLEGAL_CDA_LENGTH)
+                        }
+                    }
+
+                    return ready()
+                        .then(() =>
+                            history.batch(
+                                ops
+                                    .filter(({ type }) => type === 'deleteBundle' || type === 'deleteCDA')
+                                    .map(
+                                        ({ type, value }): PersistenceAdapterBatch => {
+                                            switch (type) {
+                                                case 'deleteBundle':
+                                                    return {
+                                                        type: 'write',
+                                                        key: tritsToBytes(bundle(value)),
+                                                        value: tritsToBytes(value),
+                                                    }
+                                                case 'deleteCDA':
+                                                    return {
+                                                        type: 'write',
+                                                        key: tritsToBytes(CDAddress(value)),
+                                                        value: tritsToBytes(value),
+                                                    }
+                                            }
+                                            /* istanbul ignore next */
+                                            return undefined as any
+                                        }
+                                    )
+                            )
+                        )
+                        .then(() =>
+                            state.batch(
+                                ops.map(
                                     ({ type, value }): PersistenceAdapterBatch => {
                                         switch (type) {
-                                            case 'deleteBundle':
+                                            case 'writeBundle':
                                                 return {
                                                     type: 'write',
                                                     key: tritsToBytes(bundle(value)),
                                                     value: tritsToBytes(value),
                                                 }
-                                            case 'deleteCDA':
+                                            case 'deleteBundle':
+                                                return {
+                                                    type: 'delete',
+                                                    key: tritsToBytes(bundle(value)),
+                                                }
+
+                                            case 'writeCDA':
                                                 return {
                                                     type: 'write',
                                                     key: tritsToBytes(CDAddress(value)),
                                                     value: tritsToBytes(value),
                                                 }
+                                            case 'deleteCDA':
+                                                return {
+                                                    type: 'delete',
+                                                    key: tritsToBytes(CDAddress(value)),
+                                                }
                                         }
-                                        /* istanbul ignore next */
-                                        return undefined as any
                                     }
                                 )
-                        )
-                    )
-                    .then(() =>
-                        state.batch(
-                            ops.map(
-                                ({ type, value }): PersistenceAdapterBatch => {
-                                    switch (type) {
-                                        case 'writeBundle':
-                                            return {
-                                                type: 'write',
-                                                key: tritsToBytes(bundle(value)),
-                                                value: tritsToBytes(value),
-                                            }
-                                        case 'deleteBundle':
-                                            return {
-                                                type: 'delete',
-                                                key: tritsToBytes(bundle(value)),
-                                            }
-
-                                        case 'writeCDA':
-                                            return {
-                                                type: 'write',
-                                                key: tritsToBytes(CDAddress(value)),
-                                                value: tritsToBytes(value),
-                                            }
-                                        case 'deleteCDA':
-                                            return {
-                                                type: 'delete',
-                                                key: tritsToBytes(CDAddress(value)),
-                                            }
-                                    }
-                                }
                             )
                         )
-                    )
-                    .tap(() => ops.forEach(({ type, value }) => this.emit(events[type], value)))
+                        .tap(() => ops.forEach(({ type, value }) => this.emit(events[type], value)))
+                },
+
+                stateRead: state.read,
+                stateWrite: state.write,
+                stateDelete: state.delete,
+                stateBatch: state.batch,
+                createStateReadStream: (options: PersistenceIteratorOptions) => state.createReadStream(options),
+
+                historyRead: history.read,
+                historyWrite: history.write,
+                historyDelete: history.delete,
+                historyBatch: history.batch,
+                createHistoryReadStream: (options: PersistenceIteratorOptions) => history.createReadStream(options),
             },
+            EventEmitter.prototype
+        )
+    }
 
-            stateRead: state.read,
-            stateWrite: state.write,
-            stateDelete: state.delete,
-            stateBatch: state.batch,
-            createStateReadStream: (options: PersistenceIteratorOptions) => state.createReadStream(options),
-
-            historyRead: history.read,
-            historyWrite: history.write,
-            historyDelete: history.delete,
-            historyBatch: history.batch,
-            createHistoryReadStream: (options: PersistenceIteratorOptions) => history.createReadStream(options),
-        },
-        EventEmitter.prototype
-    )
+    return persistenceMixin.call({})
 }
 
 export const generatePersistenceID = (seed: Int8Array): string => {
