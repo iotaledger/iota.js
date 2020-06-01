5e2501c346e965f58bd3284a67875d8d49d0ffb6
Umair Sarfraz <umair.sarfraz@iota.org>
2020-06-01 20:42:37 +0000

    fix: ignore null trytes during trytes to ascii conversion (#480)
    trytesToAscii() converts null trytes to non-breaking whitespaces. This (sometimes) lead to whitespaces at the end of ascii string.
    
    This commit fixes the issue by ignoring null trytes during trytes to ascii conversion.

--- packages/converter/src/ascii.ts
+++ packages/converter/src/ascii.ts
@@ -3,26 +3,26 @@ import * as errors from './errors'
 
 /**
  * This method converts ASCII characters to [trytes](https://docs.iota.org/docs/getting-started/0.1/introduction/ternary).
- * 
+ *
  * ## Related methods
- * 
+ *
  * To convert trytes to ASCII characters, use the [`trytesToAscii()`]{@link #module_converter.trytesToAscii} method.
- * 
+ *
  * @method asciiToTrytes
- * 
+ *
  * @summary Converts ASCII characters to trytes.
- *  
+ *
  * @memberof module:converter
  *
  * @param {string} input - ASCII input
- * 
+ *
  * @example
  * ```js
  * let trytes = Converter.asciiToTrytes('Hello, where is my coffee?');
  * ```
- * 
+ *
  * @return {string} Trytes
- * 
+ *
  * @throws {errors.INVALID_ASCII_CHARS}: Make sure that the `input` argument contains only valid ASCII characters.
  */
 export const asciiToTrytes = (input: string): string => {
@@ -45,28 +45,28 @@ export const asciiToTrytes = (input: string): string => {
 
 /**
  * This method converts trytes to ASCII characters.
- * 
+ *
  * Because each ASCII character is represented as 2 trytes, the given trytes must be of an even length.
  *
  * ## Related methods
- * 
+ *
  * To convert ASCII characters to trytes, use the [`asciiToTrytes()`]{@link #module_converter.asciiToTrytes} method.
- * 
+ *
  * @method trytesToAscii
- * 
+ *
  * @summary Converts trytes to ASCII characters.
- *  
+ *
  * @memberof module:converter
  *
  * @param {string} trytes - An even number of trytes
- * 
+ *
  * @example
  * ```js
  * let message = Converter.trytesToAscii('IOTA');
  * ```
- * 
+ *
  * @return {string} ASCII characters
- * 
+ *
  * @throws {errors.INVALID_TRYTES}: Make sure that the `trytes` argument contains only valid trytes (A-Z or 9).
  * @throws {errors.INVALID_ODD_LENGTH}: Make sure that the `trytes` argument contains an even number of trytes.
  */
@@ -82,7 +82,11 @@ export const trytesToAscii = (trytes: string): string => {
     let ascii = ''
 
     for (let i = 0; i < trytes.length; i += 2) {
-        ascii += String.fromCharCode(TRYTE_ALPHABET.indexOf(trytes[i]) + TRYTE_ALPHABET.indexOf(trytes[i + 1]) * 27)
+        const charCode = TRYTE_ALPHABET.indexOf(trytes[i]) + TRYTE_ALPHABET.indexOf(trytes[i + 1]) * 27
+
+        if (charCode) {
+            ascii += String.fromCharCode(charCode)
+        }
     }
 
     return ascii
