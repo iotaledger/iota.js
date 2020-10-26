b813c9e88f8d1753990dfd76519ad08c411c4955
Umair Sarfraz <aquadestructor@icloud.com>
2020-10-26 17:07:58 +0500

    chore: update ava across all packages

--- packages/checksum/test/checksum.test.ts
+++ packages/checksum/test/checksum.test.ts
@@ -4,11 +4,11 @@ import { addChecksum, errors, isValidChecksum, removeChecksum } from '../src'
 
 const invalidAddress = 'UYEEERFQYTPFAHIPXDQAQYWYMSMCLMGBTYAXLWFRFFWPYFOICOVLK9A9VYNCKK9TQUNBTARCEQXJHD'
 
-test('addChecksum() adds 9-trytes checksum', t => {
+test('addChecksum() adds 9-trytes checksum', (t) => {
     t.is(addChecksum(address), addressWithChecksum, 'addChecksum() should add 9-trytes checksum to the end of address.')
 })
 
-test('addChecksum() returns the exact address, if was passed with checksum', t => {
+test('addChecksum() returns the exact address, if was passed with checksum', (t) => {
     t.is(
         addChecksum(addressWithChecksum),
         addressWithChecksum,
@@ -16,24 +16,24 @@ test('addChecksum() returns the exact address, if was passed with checksum', t =
     )
 })
 
-test('addChecksum() throws error for invalid addresses', t => {
+test('addChecksum() throws error for invalid addresses', (t) => {
     const error = t.throws(
         () => addChecksum(invalidAddress),
-        Error,
+        { instanceOf: Error },
         'addChecksum() should throw error if address is invalid.'
     )
 
     t.is(error.message, errors.INVALID_ADDRESS, 'addChecksum() should throw correct error message.')
 })
 
-test('addChecksum() adds checksum of arbitrary length', t => {
+test('addChecksum() adds checksum of arbitrary length', (t) => {
     const trytes = '9'.repeat(81)
     const trytesWithChecksum = trytes + 'KZW'
 
     t.is(addChecksum(trytes, 3, false), trytesWithChecksum, 'addChecsum() should add checksum of arbitrary length.')
 })
 
-test('isValidChecksum() correctly validates the checksum', t => {
+test('isValidChecksum() correctly validates the checksum', (t) => {
     t.is(
         isValidChecksum(addressWithInvalidChecksum),
         false,
@@ -43,17 +43,17 @@ test('isValidChecksum() correctly validates the checksum', t => {
     t.is(isValidChecksum(address), false, 'isValidChecksum() should return false for address without checksum.')
 })
 
-test('isValidChecksum() throws error for invalid address', t => {
+test('isValidChecksum() throws error for invalid address', (t) => {
     const error = t.throws(
         () => isValidChecksum(invalidAddress),
-        Error,
+        { instanceOf: Error },
         'isValidChecksum() should throw error if invalid address was passed.'
     )
 
     t.is(error.message, errors.INVALID_ADDRESS, 'isValidChecksum() should throw correct error message.')
 })
 
-test('removeChecksum() removes checksum from addresses with checksum', t => {
+test('removeChecksum() removes checksum from addresses with checksum', (t) => {
     t.deepEqual(
         removeChecksum(addressWithChecksum),
         address,
@@ -61,7 +61,7 @@ test('removeChecksum() removes checksum from addresses with checksum', t => {
     )
 })
 
-test('removeChecksum() returns the exact address, if was passed without checksum', t => {
+test('removeChecksum() returns the exact address, if was passed without checksum', (t) => {
     t.deepEqual(
         removeChecksum(address),
         address,
@@ -69,10 +69,10 @@ test('removeChecksum() returns the exact address, if was passed without checksum
     )
 })
 
-test('removeChecksum() throws error for invalid addresses', t => {
+test('removeChecksum() throws error for invalid addresses', (t) => {
     const error = t.throws(
         () => removeChecksum(invalidAddress),
-        Error,
+        { instanceOf: Error },
         'removeChecksum() should throw error if invalid address was passed.'
     )
 
--- packages/validators/test/validate.test.ts
+++ packages/validators/test/validate.test.ts
@@ -1,19 +1,19 @@
 import test from 'ava'
 import { validate, Validator } from '../src'
 
-test('validate() does not throw error for valid input.', t => {
-    const validator: Validator<string> = input => [input, (v: any): v is string => typeof v === 'string', 'error']
+test('validate() does not throw error for valid input.', (t) => {
+    const validator: Validator<string> = (input) => [input, (v: any): v is string => typeof v === 'string', 'error']
 
     t.notThrows(() => validate(validator('test')), 'validate() should not throw error for valid input.')
 })
 
-test('validate() throws correct error for invalid input.', t => {
+test('validate() throws correct error for invalid input.', (t) => {
     const errorMessage = 'validation error'
-    const validator: Validator<string> = input => [input, (v: any): v is string => false, errorMessage]
+    const validator: Validator<string> = (input) => [input, (v: any): v is string => false, errorMessage]
     const value = 'test'
 
     t.is(
-        t.throws(() => validate(validator(value)), Error).message,
+        t.throws(() => validate(validator(value)), { instanceOf: Error }).message,
         `${errorMessage}: ${JSON.stringify(value, null, 1)}`,
         'validate() should throw correct error for invalid input.'
     )
