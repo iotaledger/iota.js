813e84a78efa7b14bc79da765c8e15a47e217fca
Martyn Janes <martyn@obany.com>
2021-06-14 11:25:21 +0100

    chore: Update pow-neon packaging

--- packages/pow-neon/copy-dist.js
+++ packages/pow-neon/copy-dist.js
@@ -1,1 +1,4 @@
-require("fs").copyFileSync("./native/index.node", "./dist/native/index.node")
+const fs = require("fs");
+
+fs.mkdirSync("./dist/native");
+fs.copyFileSync("./native/index.node", "./dist/native/index.node");
