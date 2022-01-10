dfe1b71b29e5c05c61e883ec3b9f1a281d02006c
Martyn Janes <martyn@obany.com>
2022-01-10 09:12:09 +0100

    Merge branch 'dev' into feat/chrysalis-p3

--- packages/util/dist/cjs/index-node.js
+++ packages/util/dist/cjs/index-node.js
@@ -437,85 +437,6 @@
 
     // Copyright 2020 IOTA Stiftung
     /**
-     * Helper methods for bigints.
-     */
-    class BigIntHelper {
-        /**
-         * Load 3 bytes from array as bigint.
-         * @param data The input array.
-         * @param byteOffset The start index to read from.
-         * @returns The bigint.
-         */
-        static read3(data, byteOffset) {
-            const v0 = (data[byteOffset + 0] + (data[byteOffset + 1] << 8) + (data[byteOffset + 2] << 16)) >>> 0;
-            return bigInt__default["default"](v0);
-        }
-        /**
-         * Load 4 bytes from array as bigint.
-         * @param data The input array.
-         * @param byteOffset The start index to read from.
-         * @returns The bigint.
-         */
-        static read4(data, byteOffset) {
-            const v0 = (data[byteOffset + 0] +
-                (data[byteOffset + 1] << 8) +
-                (data[byteOffset + 2] << 16) +
-                (data[byteOffset + 3] << 24)) >>>
-                0;
-            return bigInt__default["default"](v0);
-        }
-        /**
-         * Load 8 bytes from array as bigint.
-         * @param data The data to read from.
-         * @param byteOffset The start index to read from.
-         * @returns The bigint.
-         */
-        static read8(data, byteOffset) {
-            const v0 = (data[byteOffset + 0] +
-                (data[byteOffset + 1] << 8) +
-                (data[byteOffset + 2] << 16) +
-                (data[byteOffset + 3] << 24)) >>>
-                0;
-            const v1 = (data[byteOffset + 4] +
-                (data[byteOffset + 5] << 8) +
-                (data[byteOffset + 6] << 16) +
-                (data[byteOffset + 7] << 24)) >>>
-                0;
-            return bigInt__default["default"](v1).shiftLeft(BigIntHelper.BIG_32).or(v0);
-        }
-        /**
-         * Convert a big int to bytes.
-         * @param value The bigint.
-         * @param data The buffer to write into.
-         * @param byteOffset The start index to write from.
-         */
-        static write8(value, data, byteOffset) {
-            const v0 = Number(value.and(BigIntHelper.BIG_32_MASK));
-            const v1 = Number(value.shiftRight(BigIntHelper.BIG_32).and(BigIntHelper.BIG_32_MASK));
-            data[byteOffset] = v0 & 0xff;
-            data[byteOffset + 1] = (v0 >> 8) & 0xff;
-            data[byteOffset + 2] = (v0 >> 16) & 0xff;
-            data[byteOffset + 3] = (v0 >> 24) & 0xff;
-            data[byteOffset + 4] = v1 & 0xff;
-            data[byteOffset + 5] = (v1 >> 8) & 0xff;
-            data[byteOffset + 6] = (v1 >> 16) & 0xff;
-            data[byteOffset + 7] = (v1 >> 24) & 0xff;
-        }
-        /**
-         * Generate a random bigint.
-         * @returns The bitint.
-         */
-        static random() {
-            return BigIntHelper.read8(RandomHelper.generate(8), 0);
-        }
-    }
-    // @internal
-    BigIntHelper.BIG_32 = bigInt__default["default"](32);
-    // @internal
-    BigIntHelper.BIG_32_MASK = bigInt__default["default"](0xffffffff);
-
-    // Copyright 2020 IOTA Stiftung
-    /**
      * Convert arrays to and from different formats.
      */
     class Converter {
@@ -724,42 +645,6 @@
                         }
                     }
                 }
-            }
-        }
-    }
-
-    // Copyright 2020 IOTA Stiftung
-    // SPDX-License-Identifier: Apache-2.0
-    /**
-     * Class to help with random generation.
-     */
-    class PlatformHelper {
-    }
-    /**
-     * Is this the browser.
-     * @returns True if running in browser.
-     */
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    PlatformHelper.isNodeJs = true;
-
-    // Copyright 2020 IOTA Stiftung
-    /**
-     * Class to help with random generation.
-     */
-    class RandomHelper {
-        /**
-         * Generate a new random array.
-         * @param length The length of buffer to create.
-         * @returns The random array.
-         */
-        static generate(length) {
-            {
-                // eslint-disable-next-line @typescript-eslint/no-var-requires,@typescript-eslint/no-require-imports
-                const crypto = require("crypto");
-                return crypto.randomBytes(length);
-                // Keep this as else return so that packager keeps only one side
-                // of the if based on platform
-                // eslint-disable-next-line no-else-return
             }
         }
     }
