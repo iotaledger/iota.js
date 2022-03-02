1e45436496b6d65840a673af19d41c57dd8c4767
Martyn Janes <martyn@obany.com>
2022-03-02 16:07:17 +0100

    fix: MQTT Examples

--- packages/mqtt/examples/mqtt-client/src/index.ts
+++ packages/mqtt/examples/mqtt-client/src/index.ts
@@ -11,7 +11,7 @@ async function run() {
 
     mqttClient.milestonesConfirmed((topic, data) => console.log(topic, data));
 
-    mqttClient.messageMetadata("ec7c73e61295aba1c6ae82b06fb34964e22a8b719c008d42f8c9807fd4c8df2d", (topic, data) =>
+    mqttClient.messagesMetadata("ec7c73e61295aba1c6ae82b06fb34964e22a8b719c008d42f8c9807fd4c8df2d", (topic, data) =>
         console.log(topic, data)
     );
 
@@ -19,13 +19,13 @@ async function run() {
 
     mqttClient.messagesRaw((topic, data) => console.log(topic, data));
 
-    mqttClient.messages((topic, data, raw) => console.log(topic, data));
+    mqttClient.messages((topic, data) => console.log(topic, data));
 
-    mqttClient.tagged("aa", (topic, data, raw) => console.log(topic, data));
+    mqttClient.messagesTagged("aa", (topic, data) => console.log(topic, data));
 
-    mqttClient.taggedRaw("aa", (topic, data) => console.log(topic, data));
+    mqttClient.messagesTaggedRaw("aa", (topic, data) => console.log(topic, data));
 
-    mqttClient.messagesMetadata((topic, data) => console.log(topic, data));
+    mqttClient.messagesReferenced((topic, data) => console.log(topic, data));
 }
 
 run()
