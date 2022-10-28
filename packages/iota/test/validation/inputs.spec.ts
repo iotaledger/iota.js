27d81d99658880c630df03583ab58d0558660859
Branko Bosnic <brankobosnic1@gmail.com>
2022-10-28 15:34:18 +0200

    feat: add inputs validation

---
+++ packages/iota/test/validation/inputs.spec.ts
@@ -0,0 +1,92 @@
+// Copyright 2020 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+import { MAX_INPUT_COUNT, MIN_INPUT_COUNT } from "../../src/binary/inputs/inputs";
+import { IUTXOInput, UTXO_INPUT_TYPE } from "../../src/models/inputs/IUTXOInput";
+import { validateInput, validateInputs } from "../../src/validation/inputs/inputs";
+
+describe("Inputs validation", () => {
+    test("should pass with valid inputs", () => {
+        const inputs: IUTXOInput[] = [
+            {
+                type: UTXO_INPUT_TYPE,
+                transactionId: "0x729aac27d25a412b51fda98c07dc8a5b68b476a105ad0434389fef4109537eeb",
+                transactionOutputIndex: 0
+            },
+            {
+                type: UTXO_INPUT_TYPE,
+                transactionId: "0x729aac27d25a412b51fda98c07dc8a5b68b476a105ad0434389fef4109537eeb",
+                transactionOutputIndex: 1
+            },
+            {
+                type: UTXO_INPUT_TYPE,
+                transactionId: "0x729aac27d25a412b51fda98c07dc8a5b68b476a105ad0434389fef4109537eeb",
+                transactionOutputIndex: 2
+            },
+            {
+                type: UTXO_INPUT_TYPE,
+                transactionId: "0x729aac27d25a412b51fda98c07dc8a5b68b476a105ad0434389fef4109537bdd",
+                transactionOutputIndex: 1
+            }
+        ];
+
+        const result = validateInputs(inputs);
+        expect(result.isValid).toEqual(true);
+    });
+
+    test("should fail with maximum inputs exceeded", () => {
+        const inputs: IUTXOInput[] = [];
+
+        for (let index = 0; index < 129; index++) {
+            inputs.push({
+                type: UTXO_INPUT_TYPE,
+                transactionId: "0x729aac27d25a412b51fda98c07dc8a5b68b476a105ad0434389fef4109537eed",
+                transactionOutputIndex: index
+            });
+        }
+
+        const result = validateInputs(inputs);
+        expect(result.isValid).toEqual(false);
+        expect(result.errors).toEqual(expect.arrayContaining([`Inputs count must be between ${MIN_INPUT_COUNT} and ${MAX_INPUT_COUNT}.`]));
+    });
+
+    test("should fail on duplicate Transaction Id and Transaction Output Index pair", () => {
+        const inputs: IUTXOInput[] = [
+            {
+                type: UTXO_INPUT_TYPE,
+                transactionId: "0x729aac27d25a412b51fda98c07dc8a5b68b476a105ad0434389fef4109537eeb",
+                transactionOutputIndex: 0
+            },
+            {
+                type: UTXO_INPUT_TYPE,
+                transactionId: "0x729aac27d25a412b51fda98c07dc8a5b68b476a105ad0434389fef4109537eeb",
+                transactionOutputIndex: 0
+            },
+            {
+                type: UTXO_INPUT_TYPE,
+                transactionId: "0x729aac27d25a412b51fda98c07dc8a5b68b476a105ad0434389fef4109537eeb",
+                transactionOutputIndex: 2
+            },
+            {
+                type: UTXO_INPUT_TYPE,
+                transactionId: "0x729aac27d25a412b51fda98c07dc8a5b68b476a105ad0434389fef4109537bdd",
+                transactionOutputIndex: 1
+            }
+        ];
+
+        const result = validateInputs(inputs);
+        expect(result.isValid).toEqual(false);
+        expect(result.errors).toEqual(expect.arrayContaining(["Each pair of Transaction Id and Transaction Output Index must be unique in the list of inputs."]));
+    });
+
+    test("should fail on maximum Transaction Output Index exceeded", () => {
+        const input: IUTXOInput = {
+            type: UTXO_INPUT_TYPE,
+            transactionId: "0x729aac27d25a412b51fda98c07dc8a5b68b476a105ad0434389fef4109537eeb",
+            transactionOutputIndex: 128
+        };
+
+        const result = validateInput(input);
+        expect(result.isValid).toEqual(false);
+        expect(result.errors).toEqual(expect.arrayContaining([`Transaction Output Index must be between 0 and ${MAX_INPUT_COUNT}.`]));
+    });
+});
