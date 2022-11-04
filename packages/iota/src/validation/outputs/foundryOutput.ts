ffe536d2e9159d15421ba9ff7303bc66116de717
Branko Bosnic <brankobosnic1@gmail.com>
2022-11-04 10:28:56 +0100

    fix: add foundry oputput unlock conditions and immutable features validation

--- packages/iota/src/validation/outputs/foundryOutput.ts
+++ packages/iota/src/validation/outputs/foundryOutput.ts
@@ -1,6 +1,7 @@
 // Copyright 2020 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 import bigInt from "big-integer";
+import { IMMUTABLE_ALIAS_UNLOCK_CONDITION_TYPE } from "../../index-browser";
 import type { INodeInfoProtocol } from "../../models/info/INodeInfoProtocol";
 import { FOUNDRY_OUTPUT_TYPE, IFoundryOutput } from "../../models/outputs/IFoundryOutput";
 import { validateFeatures } from "../features/features";
@@ -8,9 +9,12 @@ import { validateNativeTokens } from "../nativeTokens";
 import { IValidationResult, mergeValidationResults } from "../result";
 import { validateUnlockConditions } from "../unlockConditions/unlockConditions";
 
+// Maximum number of unlock conditions that foundry output can have.
+const MAX_FOUNDRY_UNLOCK_CONDITIONS_COUNT = 1;
+
 /**
- * Validate a basic output.
- * @param object The object to validate.
+ * Validate a foundry output.
+ * @param foundryOutput The object to validate.
  * @param protocolInfo The Protocol Info.
  * @returns The validation result.
  */
@@ -37,24 +41,36 @@ import { validateUnlockConditions } from "../unlockConditions/unlockConditions";
             errors: ["Foundry output amount field must not be larger than max token supply."]
         });
     }
-
+    
     if (foundryOutput.nativeTokens) {
         results.push(validateNativeTokens(foundryOutput.nativeTokens));
     }
 
-    if (foundryOutput.unlockConditions) {
-        // if (!foundryOutput.unlockConditions.some(uC => uC.type === ADDRESS_UNLOCK_CONDITION_TYPE)) {
-        //     results.push({
-        //         isValid: false,
-        //         errors: ["Address Unlock Condition must be present."]
-        //     });
-        // }
-        results.push(validateUnlockConditions(foundryOutput.unlockConditions, foundryOutput.amount, protocolInfo.rentStructure));
+    if (!foundryOutput.unlockConditions || foundryOutput.unlockConditions.length !== MAX_FOUNDRY_UNLOCK_CONDITIONS_COUNT) {
+        results.push({
+            isValid: false,
+            errors: [`Foundry output unlock conditions count must be equal to ${MAX_FOUNDRY_UNLOCK_CONDITIONS_COUNT}.`]
+        });
+    } else {
+        if (foundryOutput.unlockConditions[0].type !== IMMUTABLE_ALIAS_UNLOCK_CONDITION_TYPE) {
+            results.push({
+                isValid: false,
+                errors: ["Foundry output Immutable Alias Address Unlock Condition must be present."]
+            });
+        } else {
+            results.push(validateUnlockConditions(foundryOutput.unlockConditions));
+        } 
     }
 
     if (foundryOutput.features) {
         results.push(validateFeatures(foundryOutput.features));
     }
+
+    if (foundryOutput.immutableFeatures) {
+        results.push(validateFeatures(foundryOutput.immutableFeatures));
+    }
+
+    // validateTokenScheme
 
     return mergeValidationResults(...results);
 }
