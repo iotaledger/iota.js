dcef1ac7759874568841650bdd9efbce73f9d2fe
Branko Bosnic <brankobosnic1@gmail.com>
2022-11-08 12:06:16 +0100

    fix: add Foundry output to outputs test

--- packages/iota/test/validation/outputs.spec.ts
+++ packages/iota/test/validation/outputs.spec.ts
@@ -1,16 +1,17 @@
 // Copyright 2020 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 import { validateOutputs } from "../../src/validation/outputs/outputs";
-import { cloneBasicOutput, cloneAliasOutput, cloneNftOutput } from "./testUtils";
-import { mockBasicOutput, mockNftOutput, mockAliasOutput, protocolInfoMock } from "./testValidationMocks";
+import { cloneBasicOutput, cloneAliasOutput, cloneNftOutput, cloneFoundryOutput } from "./testUtils";
+import { mockBasicOutput, mockNftOutput, mockAliasOutput, protocolInfoMock, mockFoundryOutput } from "./testValidationMocks";
 
 describe("Outputs validation", () => {
     it("should pass with valid Basic, Alias, Nft and Foundry outputs", () => {
         const basicOutput = cloneBasicOutput(mockBasicOutput);
         const aliasOutput = cloneAliasOutput(mockAliasOutput);
         const nftOutput = cloneNftOutput(mockNftOutput);
+        const foundryOutput = cloneFoundryOutput(mockFoundryOutput);
 
-        const result = validateOutputs([basicOutput, aliasOutput, nftOutput], protocolInfoMock);
+        const result = validateOutputs([basicOutput, aliasOutput, nftOutput, foundryOutput], protocolInfoMock);
 
         expect(result.isValid).toEqual(true);
         expect(result.errors).toEqual(undefined);
